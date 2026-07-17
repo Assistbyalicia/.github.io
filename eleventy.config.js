7dc5088d8e947aaf39680209f904d27c5c5fa770
Assistbyalicia <contact@assistbyalicia.fr>
2026-07-17 14:28:12 +0200

    Migration Eleventy - toutes les pages converties

---
+++ eleventy.config.js
@@ -0,0 +1,24 @@
+module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPassthroughCopy("styles.css");
+  eleventyConfig.addPassthroughCopy("includes.js");
+  eleventyConfig.addPassthroughCopy("Robots.txt");
+  eleventyConfig.addPassthroughCopy("Sitemap.xml");
+  eleventyConfig.addPassthroughCopy({ "Logo.png": "Logo.png" });
+  eleventyConfig.addPassthroughCopy("Photo de profil.png");
+  eleventyConfig.addPassthroughCopy("Capture - Audit de temps.png");
+  eleventyConfig.addPassthroughCopy("partials");
+
+  // Force les URLs en .html au lieu de /dossier/index.html
+  eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
+
+  return {
+    dir: {
+      input: "src",
+      includes: "../_includes",
+      output: "_site"
+    },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+};
