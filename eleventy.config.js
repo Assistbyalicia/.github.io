0483fc795aba726c9eac3a69aa67ef4f43d2d5d0
Assistbyalicia <contact@assistbyalicia.fr>
2026-07-17 15:30:49 +0200

    Fix chemin Logo.png

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,7 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("includes.js");
   eleventyConfig.addPassthroughCopy("Robots.txt");
   eleventyConfig.addPassthroughCopy("Sitemap.xml");
-  eleventyConfig.addPassthroughCopy({ "Logo.png": "Logo.png" });
+  eleventyConfig.addPassthroughCopy({ "src/Logo.png": "Logo.png" });
   eleventyConfig.addPassthroughCopy("Photo de profil.png");
   eleventyConfig.addPassthroughCopy("Capture - Audit de temps.png");
   eleventyConfig.addPassthroughCopy("partials");
