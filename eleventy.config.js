ce0eb8567c378490c449d57674f56facc1a0897d
Assistbyalicia <contact@assistbyalicia.fr>
2026-07-18 14:12:53 +0200

    fix: robots.txt et sitemap.xml en minuscules

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,8 +2,8 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("styles.css");
   eleventyConfig.addPassthroughCopy("includes.js");
-  eleventyConfig.addPassthroughCopy("Robots.txt");
-  eleventyConfig.addPassthroughCopy("Sitemap.xml");
+  eleventyConfig.addPassthroughCopy("robots.txt");
+  eleventyConfig.addPassthroughCopy("sitemap.xml");
   eleventyConfig.addPassthroughCopy({ "src/Logo.png": "Logo.png" });
   eleventyConfig.addPassthroughCopy("Photo de profil.png");
   eleventyConfig.addPassthroughCopy("Capture - Audit de temps.png");
